c894b20ee2f64d5b6c011666af7c19fef3435050
nicholastsk <nicholas.teh@apu.edu.my>
2024-07-14 23:20:00 +0800

    Create hotel.h

---
+++ hotel.h
@@ -0,0 +1,13 @@
+#ifndef HOTEL_H
+#define HOTEL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Shared function declarations
+void adminMenu();
+void staffMenu();
+void customerMenu();
+void guestMenu();
+
+#endif
